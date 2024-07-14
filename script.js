04f92f3526f826be903c7a3adc312593e4fabf63
Clifford Anderson <c.andersoninfotech@gmail.com>
2024-07-13 19:02:58 -0500

    created a start new task button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const addBtn = document.querySelector('.btn-send');
 const deleteBtn = document.querySelector('.btn-delete');
+const resetBtn = document.getElementById('reset');
+
 const inputArea = document.querySelector('.chore-box')
 const error = document.querySelector('.error')
 const taskArea = document.getElementById('task-container');
@@ -7,16 +9,23 @@ const createdTasks = document.querySelectorAll('.created-task');
 
 let tasksArray = [];
 let items = '';
+deleteBtn.disabled = true;
 
 addBtn.addEventListener('click', () => { addTask(createdTasks);});
-deleteBtn.addEventListener('dblclick', () => { removeAllTasks ();});
+deleteBtn.addEventListener('dblclick', () => { removeAllTasks();});
+resetBtn.addEventListener('click', () => { reloadList();});
+
+// Added 'Enter' functionality
 
 inputArea.addEventListener('keypress', function(e) {
     if( e.key === 'Enter' ) {
         e.preventDefault();
         addBtn.click();
+        enableButtons();
     }
 })
+
+// Get items from localStorage
 
 retrieveStoredItems();
 
@@ -28,6 +37,7 @@ function addTask() {
             tasksArray.push(inputArea.value);
             renderTasks();
             inputArea.value = '';
+            enableButtons();
     } else if( tasksArray.includes(inputArea.value) ) {
         error.innerHTML = `<div class='error'>Duplicates not allowed!</div>`
     } else {
@@ -44,14 +54,18 @@ function addTask() {
     localStorage.setItem('myTasks', JSON.stringify(tasksArray))
 }
 
-// Remove all tasks
+// Remove all tasks and allow restart
 
 function removeAllTasks() {
     tasksArray = [];
     renderTasks();
     inputArea.value = ''
     localStorage.clear();
-}
+    taskArea.innerHTML = `<img src="images/giphy.webp" />
+                          <h2 class='congratulations'>Great Job!  You've finished all of your tasks!!</h2>
+                         `
+    resetBtn.classList.remove('hide');
+};   
 
 
 // Render items to browser
@@ -68,13 +82,28 @@ function renderTasks() {
     taskArea.innerHTML = taskItems;
 }
 
+function enableButtons() {
+    if( tasksArray.length === 0 ) {
+        deleteBtn.disabled = true;
+    } else { 
+        deleteBtn.disabled = false;
+    }
+}
 
-// Retrieve stored items from localStorage and render them
+// Start new list
+
+function reloadList() {
+    location.reload();
+}
+
+
+// Retrieve stored items from localStorage and conditionally render them
 
 function retrieveStoredItems() {
     
     if( localStorage.getItem('myTasks') ) {
         tasksArray = JSON.parse(localStorage.getItem('myTasks'));
+        deleteBtn.disabled = false;
         renderTasks();
     }
 }
